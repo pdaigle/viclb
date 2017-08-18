7280adc73d839f1c301a9ece01b509b231d9842f
Patrick Daigle <pdaigle@pdaigle-mbair.local>
2017-08-18 11:58:37 -0400

    lb_config.sh now generates complete haproxy.cfg, removed haproxy.cfg.tmpl file

--- lb_config.sh
+++ lb_config.sh
@@ -1,36 +1,61 @@
 #!/bin/bash
 
-# define number of web server instances
-NUM_WEB_INSTANCES=3
-# name of routable (external) network
-EXTERNAL_NET=routable
+## USER-DEFINED VARIABLES
+# docker image containing the service to be load-balanced
+INSTANCE_IMG=httpd
+# number of service instances desired
+NUM_INSTANCES=3
+# port running on instance
+INSTANCE_PORT=80
+# port to be exposed on load-balancer
+LB_PORT=80
 # container names
-WEB_CTR_PREFIX=web
+INSTANCE_CTR_PREFIX=web
 LB_CTR_NAME=lb
+# name of routable (external) network
+# this needs to be defined on your VCH using the '--container-network' option
+# use 'docker network ls' to list available external networks
+EXTERNAL_NET=routable
 
-# initialize haproxy.cfg with template
-cp haproxy.cfg.tmpl haproxy.cfg
+## NO NEED TO MODIFY BEYOND THIS POINT
+# initialize haproxy.cfg with defaults
+echo -e global'\n'\
+'\t'maxconn\ 256'\n'\
+'\t'log\ 127.0.0.1:514\ local0'\n'\
+defaults'\n'\
+'\t'mode\ http'\n'\
+'\t'timeout\ connect\ 5000ms'\n'\
+'\t'timeout\ client\ 50000ms'\n'\
+'\t'timeout\ server\ 5000ms'\n'\
+'\t'log\ global'\n'\
+frontend\ http-in'\n'\
+'\t'bind\ \*:$LB_PORT'\n'\
+'\t'default_backend\ servers'\n'\
+backend\ servers > haproxy.cfg
 
-# pull the images first
-docker pull httpd
+# pull the images
+docker pull $INSTANCE_IMG
 docker pull haproxy:1.7
 
-# create a user defined bridge for the web services. This will fail if the network already exists.
-docker network create web-net
+# create a user-defined bridge for the services
+# note - this will fail if the network already exists
+docker network create $INSTANCE_CTR_PREFIX-net
 
-# start the number of web instances defined above
-for i in $(seq "${NUM_WEB_INSTANCES}"); do
+# start the number of instances defined above
+for i in $(seq "${NUM_INSTANCES}"); do
   # run web instance
-  docker run -d -p 80 --name $WEB_CTR_PREFIX-${i} --net web-net httpd
+  docker run -d -p $INSTANCE_PORT --name $INSTANCE_CTR_PREFIX-${i} --net $INSTANCE_CTR_PREFIX-net $INSTANCE_IMG
   # add entry in load-balancer config file
-  echo -e '\t'server\ server1\ $WEB_CTR_PREFIX-${i}:80\ maxconn\ 32 >> haproxy.cfg
+  echo -e '\t'server\ server1\ $INSTANCE_CTR_PREFIX-${i}:$INSTANCE_PORT\ maxconn\ 32 >> haproxy.cfg
 done
 
-# start the load-balancer - note it resolves the web instances by name over web-net
+# create the load-balancer
+# connect it to a routable network where it will expose the service
 docker create --name $LB_CTR_NAME --net $EXTERNAL_NET haproxy:1.7
-# connect to web-net for communications with web instances
-docker network connect web-net $LB_CTR_NAME
-# copy haproxy config file to container before starting it
+# connect to user-defined bridge network
+# note - it resolves the instances by name over this network
+docker network connect $INSTANCE_CTR_PREFIX-net $LB_CTR_NAME
+# copy haproxy config file to the container
 docker cp haproxy.cfg $LB_CTR_NAME:/usr/local/etc/haproxy/haproxy.cfg
-
+# start the load-balancer
 docker start $LB_CTR_NAME
